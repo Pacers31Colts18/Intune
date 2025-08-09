bf59b78b55f45b5f99c6bdf8f03df9cf5adef13d
Joe Loveless <jloveless03@pm.me>
2025-08-09 12:32:04 -0500

    Create Detection_WHFBProfileCount.ps1

---
+++ Detection_WHFBProfileCount.ps1
@@ -0,0 +1,26 @@
+﻿# Define profiles to exclude
+$excludedProfiles = @("Administrator", "Guest", "Public", "Default", "defaultuser0")
+$DaysAgo = (Get-Date).AddDays(-90)
+
+# Get user profiles
+$userProfiles = Get-WmiObject -Class Win32_UserProfile | Where-Object {
+   ($_.Special -eq $false) -and
+   ($excludedProfiles -notcontains $_.LocalPath.Split('\')[-1]) -and
+   ($_.LastUseTime -gt $DaysAgo)
+}
+
+# Check profile count
+if ($userProfiles.count -le 10) {
+   Write-Output "Compliant: Less than 10 profiles found, $($userProfiles.count) total profiles found."
+   Write-Output "Profiles found:"
+   $userProfiles | ForEach-Object { Write-Output $_.LocalPath.Split('\')[-1] }
+   Exit 1
+} 
+else {
+   Write-Output "Non-Compliant: More than 10 profiles found, $($userProfiles.count) total profiles found."
+   Write-Output "Profiles found:"
+   $userProfiles | ForEach-Object { Write-Output $_.LocalPath.Split('\')[-1] }
+   Exit 0
+}
+
+
